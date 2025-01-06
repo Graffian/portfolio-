61cc5730b253becf2c4f896cbf8bde70ab78290f
ayushkant <rudrabehera2006@gmail.com>
2025-01-06 20:05:22 +0530

    al file changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import Database from './database/db.js'
+import SignIn from './login/signin.js'
+import SignUp from './login/signup.js'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Main from './notes/main.js'
+import Note from './notes/note.js'
 function App(){
   
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<SignIn/>
+    },
+    {
+      path:"/signup",
+      element:<SignUp/>
+    },
+    {
+      path:"/main",
+      element:<Main/>
+    },
+    {
+      path:"/note",
+      element:<Note/>
+    }
+    ])
   return(
-    <Database/>
+    
+    <RouterProvider router={router}/>
+    
   )
 }
 export default App;

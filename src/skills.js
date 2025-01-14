3b6d2250a600eb0ac3b13811e29d847bd1024813
Ayushhh <89790082+Graffian@users.noreply.github.com>
2025-01-14 12:48:36 +0530

    skills.js

--- src/skills.js
+++ src/skills.js
@@ -24,7 +24,7 @@ function Skill(){
     };githubApi()
   },[])
   return(<div>
-       <h1>AYUSH'S WEBSITE</h1>
+       <h1>AYUSH'S PORTFOLIO WEBSITE</h1>
       <nav className="navigation">
         <Link className="navElement" to="/">HOME</Link>
         <Link className="navElement" to="/skills">SKILLS AND PROJECTS</Link>
